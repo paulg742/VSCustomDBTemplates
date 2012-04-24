6c100f6c5f7ac0674e9437dfd32e8f9cb4f81124
Paul Van Gorp <paul_van_gorp@yahoo.com.au>
2012-04-24 16:26:18 +1000

    updated foreign key template

--- Create Foreign Key 2008.sql
+++ Create Foreign Key 2008.sql
@@ -1,5 +1,10 @@
-﻿ALTER TABLE [$SchemaName$].[$ParentObjectName$]
-	ADD CONSTRAINT [$ChildObjectName$] 
+﻿--**********************************************************
+--*
+--*		$SchemaQualifiedObjectName$	- [Description]
+--*
+
+ALTER TABLE $ParentObjectName$
+	ADD CONSTRAINT $ChildObjectName$
 	FOREIGN KEY (column_1)
 	REFERENCES tableName (column_1)	
 
